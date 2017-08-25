a2d115f686d2db911e4a2b767e1ac1e95d48427c
chrismhurst <chritterfur@gmail.com>
2017-08-25 15:32:50 -0400

    re-added middleware for public static content

--- server.js
+++ server.js
@@ -21,6 +21,10 @@ app.use((req, res, next) => {
 //middleware for letsencrypt
 app.use('/.well-known/acme-challenge', letsEncrypt);
 
+//serve static content dynamically from /public
+app.use(express.static(__dirname + '/public'));
+
+
 //manual path for letsencrypt !fix me later! (in case this isn't fixed by the next renewal, just add another app.get path and move on)
 // app.get('/.well-known/acme-challenge/:id', (req, res) => {
 //   res.send('24bpJ6_K_tE6MD0NTdEtcBAisyxfd7ZsrB2_-gCWtEs.Oc9qqVSPRacPuT0q-3JMqktI6EmK16zrNWzgmy1JpAo')
