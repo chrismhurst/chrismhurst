c1651fa42ebdefc746c401ee5443e1b3626f2348
chrismhurst <chritterfur@gmail.com>
2017-08-25 11:22:37 -0400

    still trying to get letsencrypt working in google apps

--- server.js
+++ server.js
@@ -18,6 +18,9 @@ app.use((req, res, next) => {
 //tells express that static site content can be found at /public
 app.use(express.static(__dirname + '/public'));
 
+//tells express that secondary static site content can be found at /well-known (for letsencrypt)
+app.use(express.static(__dirname + '/well-known'));
+
 //starts node.js app on port
 app.listen(port, () => {
   console.log(`Server has started on port ${port}`);
