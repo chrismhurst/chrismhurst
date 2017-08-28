3d15703ea786a85f3b0243f895cc3baf25f28d47
chrismhurst <chritterfur@gmail.com>
2017-08-28 13:45:27 -0400

    changed the letsencrypt authentication method to look at a google storage bucket instead

--- config/letsEncrypt.js
+++ config/letsEncrypt.js
@@ -2,21 +2,36 @@
 var express = require('express');
 var router = require('express').Router();
 
-//set constants for encryption from letsencrypt command
-const letsEncryptPath = 'mVGn8u8ig4lhC2vAw1Zdeij01Tp_8DPl4WAfuzgi5uY';
-const letsEncryptData = 'mVGn8u8ig4lhC2vAw1Zdeij01Tp_8DPl4WAfuzgi5uY.LToI9ULE3ZWttdyea0mmptp7V5-4nBbh847mSuHamJs';
-const wwwletsEncryptPath = 'zfFTa2M2qEUyxfO7DyB9c-0aEl4YyIDIolAOtR1H-1o';
-const wwwletsEncryptData = 'zfFTa2M2qEUyxfO7DyB9c-0aEl4YyIDIolAOtR1H-1o.LToI9ULE3ZWttdyea0mmptp7V5-4nBbh847mSuHamJs';
+//URL for gcloud documentation = https://googlecloudplatform.github.io/google-cloud-node/#/docs/storage/1.2.0/storage/file
+//import gcloud storage module
+var gcs = require('@google-cloud/storage')();
 
-//make router path for letsencrypt
-router.get(`/${letsEncryptPath}`, (req, res) => {
-  res.send(letsEncryptData);
-})
+//set bucket google bucket
+const gcsBucket = gcs.bucket('acme-challenges-99uvtcrc58gsg98.chrismhurst.com');
 
-//make router path for www pathed site
-router.get(`/${wwwletsEncryptPath}`, (req, res) => {
-  res.send(wwwletsEncryptData);
-})
+//function to return the contents of the google file
+var returnGcloudFileContents = (path) => {
+  return new Promise((resolve, reject) => {
+    var file = gcsBucket.file(path);
+    console.log(file);
+    file.download().then((fileData) => {
+        resolve(fileData[0]);
+      }).catch((err) => {
+        reject(`Attempt to access file returned error: ${err.code} - ${err.message}`);
+      });
+  });
+};
+
+//turn path into param, and pass it to the returnGcloudFileContentsAsString function
+router.get(`/:path`, (req, res) => {
+  returnGcloudFileContents(req.params.path).then((gcloudFile) => {
+    console.log(`Returned letsencrypt key: ${gcloudFile.toString()}`);
+    res.send(gcloudFile);
+  }).catch((err) => {
+    console.log(err);
+    res.send(err);
+  });
+});
 
 //export express routes
 module.exports = router;
